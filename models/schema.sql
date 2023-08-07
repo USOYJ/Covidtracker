6998e436537691b2bf6a8ccfadad103fee381050
akash301994 <patel.akash30@icloud.com>
2023-08-07 16:36:52 -0400

    added folders

--- models/schema.sql
+++ models/schema.sql
@@ -1,29 +1,29 @@
-DROP DATABASE IF EXISTS child_db;
-CREATE DATABASE child_db;
+-- DROP DATABASE IF EXISTS child_db;
+-- CREATE DATABASE child_db;
 
-USE child_db;
+-- USE child_db;
 
-CREATE TABLE schedule (
-  id INT NOT NULL,
-  first_name VARCHAR(30),
-  last_name VARCHAR(30),
-  PRIMARY KEY (id)
-);
+-- CREATE TABLE schedule (
+--   id INT NOT NULL,
+--   first_name VARCHAR(30),
+--   last_name VARCHAR(30),
+--   PRIMARY KEY (id)
+-- );
 
-CREATE TABLE courses (
-  id INT,
-  course_title VARCHAR(30) NOT NULL,
-  instructor_id INT,
-  order_details TEXT,
-  FOREIGN KEY (instructor_id)
-  REFERENCES instructors(id)
-  ON DELETE SET NULL
-);
+-- CREATE TABLE courses (
+--   id INT,
+--   course_title VARCHAR(30) NOT NULL,
+--   instructor_id INT,
+--   order_details TEXT,
+--   FOREIGN KEY (instructor_id)
+--   REFERENCES instructors(id)
+--   ON DELETE SET NULL
+-- );
 
-ROP DATABASE IF EXISTS course_db;
-CREATE DATABASE course_db;
+-- ROP DATABASE IF EXISTS course_db;
+-- CREATE DATABASE course_db;
 
-USE course_db;
+-- USE course_db;
 
 
 
--- models/seeds.sql
+++ models/seeds.sql
@@ -1,5 +1,5 @@
-INSERT INTO child (id, firstname, lastname, parentemail, password, active)
-VALUES (001, "Ella", "Jones", "michealjones@pmail.com", "Mjon123@", true),
-       (002, "Simbi", "Adeyemi", "sallyadeyimi@pmail.com", "@saL87L3!", true),
-       (003, "Brandon", "Alexander", "fionaaex@pmail.com", "0$Br71!!", true),
-       (004, "Akash", "Patel", "suneethapatel@pmail.com", "B3t%an1y", true);
+-- INSERT INTO child (id, firstname, lastname, parentemail, password, active)
+-- VALUES (001, "Ella", "Jones", "michealjones@pmail.com", "Mjon123@", true),
+--        (002, "Simbi", "Adeyemi", "sallyadeyimi@pmail.com", "@saL87L3!", true),
+--        (003, "Brandon", "Alexander", "fionaaex@pmail.com", "0$Br71!!", true),
+--        (004, "Akash", "Patel", "suneethapatel@pmail.com", "B3t%an1y", true);

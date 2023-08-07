efb651c89818a1a59cb286b09a4020027097a3b0
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-08-07 16:21:55 -0400

    added seeders

---
+++ seeders/seeds.js
@@ -0,0 +1,25 @@
+const sequelize = require('../config/connection');
+const { Child, Schedule } = require('../models');
+
+const childData = require('./childData.json');
+const scheduleData = require('./scheduleData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  const child = await Child.bulkCreate(childData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  for (const schedule of scheduleData) {
+    await Schedule.create({
+      schedule,
+      child_id: child[Math.floor(Math.random() * child.length)].id,
+    });
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();

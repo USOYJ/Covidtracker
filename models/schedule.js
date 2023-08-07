825f57ffa02ccc64196f60490b5194d571db1821
akash301994 <patel.akash30@icloud.com>
2023-08-07 15:58:28 -0400

    fixed scheudle model

--- models/schedule.js
+++ models/schedule.js
@@ -1,16 +1,46 @@
-module.exports = function(sequelize, DataTypes) {
-  var Schedule = sequelize.define('Schedule', {
-    monday: DataTypes.BOOLEAN,
-    tuesday: DataTypes.BOOLEAN,
-    wednesday: DataTypes.BOOLEAN,
-    thursday: DataTypes.BOOLEAN,
-    friday: DataTypes.BOOLEAN
-  });
-  Schedule.associate = function(models) {
-    Schedule.belongsTo(models.Child, {
-      foreignKey: {allowNull: true,defaultValue:0}
-    });
-  };
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
 
-  return Schedule;
-};
+class Schedule extends Model {
+
+}
+Schedule.init(
+  {
+    monday: {type:DataTypes.BOOLEAN},
+    tuesday: {type:DataTypes.BOOLEAN},
+    wednesday: {type:DataTypes.BOOLEAN},
+    thursday: {type:DataTypes.BOOLEAN},
+    friday: {type:DataTypes.BOOLEAN},
+    child_id:{
+      type:DataTypes.INTEGER,
+      references:{
+        model: 'child',
+        key:'id',
+      }
+    }
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'schedule',
+  }
+);
+module.exports = Schedule;
+// module.exports = function(sequelize, DataTypes) {
+//   var Schedule = sequelize.define('Schedule', {
+//     monday: DataTypes.BOOLEAN,
+//     tuesday: DataTypes.BOOLEAN,
+//     wednesday: DataTypes.BOOLEAN,
+//     thursday: DataTypes.BOOLEAN,
+//     friday: DataTypes.BOOLEAN
+//   });
+//   Schedule.associate = function(models) {
+//     Schedule.belongsTo(models.Child, {
+//       foreignKey: {allowNull: true,defaultValue:0}
+//     });
+//   };
+
+//   return Schedule;
+// };

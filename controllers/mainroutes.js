19d5d112b88d25884ba627fb3c3ee6d096104f32
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-08-12 14:09:08 -0400

    Changes to home

--- controllers/mainroutes.js
+++ controllers/mainroutes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-const {User, Child, Schedule} = require('../models');
+const { Child, Schedule} = require('../models');
 
 
 router.get('/', function (req, res) {

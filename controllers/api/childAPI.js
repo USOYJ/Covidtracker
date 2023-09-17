0fdfa7c89661b74ade89580cfb782d45b8cdf537
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-17 17:11:25 -0400

    Saved progress

--- controllers/api/childAPI.js
+++ controllers/api/childAPI.js
@@ -32,16 +32,13 @@ router.get('/new', function (req, res) {
 
 //add new child
 router.post('/', function (req, res) {
-  //console.log(req.body);
-  db.Child.create(req.body).then(function (dbChild) {
-    //console.log(dbChild);
-    //res.json(dbChild);
-    res.render('schedulechild', {
-      childid: dbChild.id,
-      pid: req.body.ParentId,
-      //change to parent email
+  db.Child.create(req.body)
+    .then(function () {
+      res.redirect('/home');
+    })
+    .catch(function (err) {
+      res.json(err);
     });
-  });
 });
 
 router.post('/:id', function (req, res) {

11d3c1893dd3a20afad77a9db891f1f575ea65b4
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-09-17 18:10:17 -0400

    Deleted stuff from the seeding folder

--- seeders/seeds.js
+++ seeders/seeds.js
@@ -1,36 +1,27 @@
 const sequelize = require('../config/connection');
-const seedFirstnames = require('./firstname');
-const seedLastnames = require('./lastname');
-const seedParentsemail = require('./parentsemail');
-const seedParentsphone = require('./parentsphone');
+const { Child } = require('../models');
+
 const childData = require('./childData.json');
 
-const seedAll = async () => {
-  try {
-    await sequelize.sync({ force: true });
-    await seedFirstnames();
-    await seedLastnames();
-    await seedParentsemail();
-    await seedParentsphone();
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-    // Seeding childData or other data, if needed
-    const children = await Child.bulkCreate(childData, {
-      individualHooks: true,
-      returning: true,
+  const children = await Child.bulkCreate(childData, {
+    individualHooks: true,
+    returning: true,
+  });
+
+  // Inserting the children data into the child table
+  for (const child of children) {
+    await Child.create({
+      ...child,
     });
-
-    console.log(`Successfully seeded ${children.length} children.`);
-    console.log('Successfully seeded firstname, lastname, parentsemail, parentsphone, etc.');
-
-    console.log('Database seeded successfully!');
-  } catch (error) {
-    console.error('Error seeding the database:', error);
-  } finally {
-    process.exit(0);
   }
+
+  process.exit(0);
 };
 
-seedAll();
+seedDatabase();
 
 
 

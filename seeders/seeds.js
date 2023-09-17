42d68916003523bf49a1c88d496221f850f6d441
Oyin <125850331+USOYJ@users.noreply.github.com>
2023-09-17 18:08:37 -0400

    seed

--- seeders/seeds.js
+++ seeders/seeds.js
@@ -1,36 +1,21 @@
+/* eslint-disable no-unused-vars */
 const sequelize = require('../config/connection');
-const seedFirstnames = require('./firstname');
-const seedLastnames = require('./lastname');
-const seedParentsemail = require('./parentsemail');
-const seedParentsphone = require('./parentsphone');
+const { Child} = require('../models');
+
 const childData = require('./childData.json');
 
-const seedAll = async () => {
-  try {
-    await sequelize.sync({ force: true });
-    await seedFirstnames();
-    await seedLastnames();
-    await seedParentsemail();
-    await seedParentsphone();
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
 
-    // Seeding childData or other data, if needed
-    const children = await Child.bulkCreate(childData, {
-      individualHooks: true,
-      returning: true,
-    });
+  const children = await Child.bulkCreate(childData, {
+    individualHooks: true,
+    returning: true,
+  });
 
-    console.log(`Successfully seeded ${children.length} children.`);
-    console.log('Successfully seeded firstname, lastname, parentsemail, parentsphone, etc.');
-
-    console.log('Database seeded successfully!');
-  } catch (error) {
-    console.error('Error seeding the database:', error);
-  } finally {
-    process.exit(0);
-  }
+  process.exit(0);
 };
 
-seedAll();
+seedDatabase();
 
 
 
